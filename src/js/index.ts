ea78b1fb73a4e7e8733df204bc726d78790c5e62
qp0kycHuk <serega990112@mail.ru>
2024-08-28 10:12:01 +0300

    add copy button

--- src/js/index.ts
+++ src/js/index.ts
@@ -40,3 +40,75 @@ window.addEventListener('scroll', scrollHandler)
 function scrollHandler() {
   document.body.classList.toggle('scroll-top', window.scrollY == 0)
 }
+
+//  script for copy code
+
+
+
+(() => {
+  const timeOuts = new Map()
+
+  document.addEventListener('click', (event) => {
+    const copyButton = (event.target as HTMLElement)?.closest('[data-copy]')
+
+    if (!copyButton) return
+
+    copyTextToClipboard(copyButton.getAttribute('data-copy') || '')
+
+    copyButton.classList.add('active')
+
+    if (timeOuts.has(copyButton)) {
+      clearTimeout(timeOuts.get(copyButton))
+    }
+
+    timeOuts.set(
+      copyButton,
+      setTimeout(() => {
+        copyButton.classList.remove('active')
+      }, 2000)
+    )
+  })
+
+  function fallbackCopyTextToClipboard(text: string) {
+    const textArea = document.createElement('textarea')
+
+    textArea.value = text
+
+    // Avoid scrolling to bottom
+    textArea.style.top = '0'
+    textArea.style.left = '0'
+    textArea.style.position = 'fixed'
+
+    document.body.appendChild(textArea)
+    textArea.focus()
+    textArea.select()
+
+    try {
+      const successful = document.execCommand('copy')
+      const msg = successful ? 'successful' : 'unsuccessful'
+
+      console.log('Fallback: Copying text command was ' + msg)
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err)
+    }
+
+    document.body.removeChild(textArea)
+  }
+
+  function copyTextToClipboard(text: string) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text)
+
+      return
+    }
+
+    navigator.clipboard.writeText(text).then(
+      function () {
+        console.log('Async: Copying to clipboard was successful!')
+      },
+      function (err) {
+        console.error('Async: Could not copy text: ', err)
+      }
+    )
+  }
+})()
